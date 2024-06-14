c55f32890dfa47249e2ae7286768fb3f52e81334
Aleeg0 <Sankafoxe@gmail.com>
2024-06-14 23:11:57 +0300

    add initBoard methods

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,5 +1,11 @@
 import {Cell} from "./Cell";
 import {Colors} from "./Colors";
+import {King} from "./figures/King";
+import {Pawn} from "./figures/Pawn";
+import {Queen} from "./figures/Queen";
+import {Rook} from "./figures/Rook";
+import {Knight} from "./figures/Knight";
+import {Bishop} from "./figures/Bishop";
 
 export class Board {
     cells: Cell[][] = [];
@@ -15,5 +21,52 @@ export class Board {
             }
             this.cells.push(row);
         }
+    }
+
+    private initPawns(){
+        for (let i = 0; i < this.COUNT_OF_CELL; i++) {
+            new Pawn(this.cells[1][i],Colors.BLACK);
+            new Pawn(this.cells[6][i],Colors.WHITE);
+        }
+    }
+
+    private initRooks(){
+        new Rook(this.cells[0][0],Colors.BLACK);
+        new Rook(this.cells[0][7],Colors.BLACK);
+        new Rook(this.cells[7][0],Colors.WHITE);
+        new Rook(this.cells[7][7],Colors.WHITE);
+    }
+
+    private initKnight(){
+        new Knight(this.cells[0][1],Colors.BLACK);
+        new Knight(this.cells[0][6],Colors.BLACK);
+        new Knight(this.cells[7][1],Colors.WHITE);
+        new Knight(this.cells[7][6],Colors.WHITE);
+    }
+
+    private initBishop(){
+        new Bishop(this.cells[0][2],Colors.BLACK);
+        new Bishop(this.cells[0][5],Colors.BLACK);
+        new Bishop(this.cells[7][2],Colors.WHITE);
+        new Bishop(this.cells[7][5],Colors.WHITE);
+    }
+
+    private initQueens(){
+        new Queen(this.cells[0][3],Colors.BLACK);
+        new Queen(this.cells[7][3],Colors.WHITE);
+    }
+
+    private initKings(){
+        new King(this.cells[0][4],Colors.BLACK);
+        new King(this.cells[7][4],Colors.WHITE);
+    }
+
+    public initFigures(){
+        this.initPawns();
+        this.initRooks();
+        this.initKnight();
+        this.initBishop();
+        this.initQueens();
+        this.initKings();
     }
 }
