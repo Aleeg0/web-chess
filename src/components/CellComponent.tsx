19eebeeae2e63dac263d2053e73af30b8ef06279
Aleeg0 <Sankafoxe@gmail.com>
2024-06-15 19:14:54 +0300

    add effect to cells

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,15 +3,22 @@ import {Cell} from "../models/Cell";
 
 interface CellProps {
     cell: Cell;
+    isSelected: boolean;
+    click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell,isSelected,click}) => {
     return (
-        <div className= {["cell", cell.color].join(" ")}>
+        <div className= {["cell", cell.color, isSelected ? "selected" : "",
+            cell.isAvailable && cell.figure ? "availableFigure" : ""].join(" ")}
+             onClick={() => click(cell)}
+        >
+            {cell.isAvailable && !cell.figure && <div className="available"/> }
             {cell.figure?.logo &&
                 <img src={cell.figure.logo}
                      alt={cell.figure.name ? cell.figure.name : ""}
-                />}
+                />
+            }
         </div>
     );
 };
